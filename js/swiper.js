76d4391a555a5318e2712a811ae00342ab2fb6ae
tsuroki <tsuroki0210@gmail.com>
2026-01-05 10:50:16 +0900

    guide book site

--- js/swiper.js
+++ js/swiper.js
@@ -1,1 +1,9 @@
 /* swiper js - horizen */
+const swiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  pagination:{
+    el: ".guide-nav",
+    type: "fraction"
+  }
+})
